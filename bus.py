b296c9f12da080d5c31e07ef59b91999cdc4ee34
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-10-07 16:14:01 -0400

    Update bus.py

--- bus.py
+++ bus.py
@@ -4,6 +4,10 @@ Demonstrate a dictionary.
 """
 
 import sys
+import os
+import tempfile
+import playsound
+import gtts   #Google Text-To-Speech
 
 bus = {
     "wheels": "round and round",
@@ -16,9 +20,30 @@ bus = {
     "muggers": "bang, bang, bang"
 }
 
+song = ""
+song3 = " All through the town"
 for things in bus:
-    print(f"The {things} on the bus go {bus[things]}")
-    print(f"{bus[things]}")
-    print(f"{bus[things]}")
-    print(f"The {things} on the bus go {bus[things]}")
-    print("All through the town")
+    song1 = " The " + things + " on the bus go " + bus[things]
+    song2 = " " + bus[things] + "," + bus[things]
+    song = song + song1 + song2 + song3
+    
+try:
+    textToSpeech = gtts.gTTS(text = song, lang = lang, slow = True)
+except BaseException as error:
+    print(error, file = sys.stderr)
+    sys.exit(1)
+
+# Save the audio in a temporary file with a name.
+temporaryFile = tempfile.NamedTemporaryFile()
+textToSpeech.save(temporaryFile.name)
+
+# Play and erase the temporary file.
+try:
+    playsound.playsound(temporaryFile.name, True)   #Requires a filename or URL.
+except OSError as error:
+    print(error, file = sys.stderr)
+    sys.exit(1)
+finally:
+    temporaryFile.close()   #Erases the temporary file.
+
+sys.exit(0)
